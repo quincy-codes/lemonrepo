59cbafd699c8061f3e2060d302f99a5e72020f00
Quincy Adams <qadams@fortissolutionsgroup.com>
2024-11-14 12:56:34 -0500

    Style Fixes

--- app/pages/evolve-api-documentation.vue
+++ app/pages/evolve-api-documentation.vue
@@ -1,17 +1,17 @@
 <template>
-  <div class="max-w-xl mx-auto">
+  <div class="max-w-4xl mx-auto">
     <UContainer class="max-w-8xl py-12 space-y-6">
-        <h1 class="text-4xl sm:text-6xl lg:text-7xl tracking-tight text-gray-800 font-bold text-center font-display max-w-4xl mx-auto">
-          Evolve Documentation
-        </h1>
+      <h1 class="text-4xl sm:text-6xl lg:text-7xl tracking-tight text-gray-800 font-bold text-center font-display max-w-4xl mx-auto">
+        Evolve Documentation
+      </h1>
 
       <div class="w-full columns-3 py-4" >
-        <ButtonLink label="Core" size="sm" rounded="sm" href="/evolve-core-documentation" />
-        <ButtonLink label="API" size="sm" rounded="sm" href="/evolve-api-documentation" />
-        <ButtonLink label="UI" size="sm" rounded="sm" href="/evolve-UI-documentation" />
+        <ButtonLink label="Core" size="sm" rounded="md" href="/evolve-core-documentation" />
+        <ButtonLink label="API" size="sm" rounded="md" href="/evolve-api-documentation" />
+        <ButtonLink label="UI" size="sm" rounded="md" href="/evolve-UI-documentation" />
       </div>
 
-        <h3 class="max-w-xl text-2xl sm:text-2xl font-semibold tracking-tight text-gray-900 font-display mb-4 mt-8">
+      <h3 class="text-center max-w-xl text-2xl sm:text-2xl font-semibold tracking-tight text-gray-900 font-display mb-4 mt-8">
           EvolveUI (Extension / Prerelease)
         </h3>
 
