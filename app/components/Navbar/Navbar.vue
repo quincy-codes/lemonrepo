3778b3b2d97d6153c39316de17b7dd6585c973dc
Quincy Adams <qadams@fortissolutionsgroup.com>
2024-11-14 22:17:07 -0500

    Update Core as its a Private Package

--- app/components/Navbar/Navbar.vue
+++ app/components/Navbar/Navbar.vue
@@ -19,7 +19,7 @@
             :to="productUrl"
             class="shiny-button px-6 py-2"
           >
-            Buy now for $149
+            Buy Now for $149
           </UButton>
         </ScriptLemonSqueezy>
       </div>
