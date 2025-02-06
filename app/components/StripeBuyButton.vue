48580b476e88e2c49b8183d63a8d5a8a0602586f
Quincy Adams <iam@quincy.codes>
2025-02-06 09:03:01 -0500

    Refactor Stripe button and remove modal
    - Changed the Stripe buy button to link directly to the payment page.
    - Removed the modal component that was previously used for checkout.

--- app/components/StripeBuyButton.vue
+++ app/components/StripeBuyButton.vue
@@ -1,29 +1,16 @@
 <template>
-  <div>
-    <UButton
-      color="black"
-      :ui="{ rounded: 'rounded-full' }"
-      class="shiny-button px-6 py-2"
-      @click="isModalOpen = true"
-    >
-      Buy Now for $149
-    </UButton>
-
-    <UModal v-model="isModalOpen">
-      <div class="w-full h-[600px]">
-        <iframe 
-          src="https://buy.stripe.com/7sI9CH0x17bb6aYeUV"
-          class="w-full h-full border-0"
-          frameborder="0"
-          allowpaymentrequest
-        ></iframe>
-      </div>
-    </UModal>
-  </div>
+  <UButton
+    color="black"
+    :ui="{ rounded: 'rounded-full' }"
+    class="shiny-button px-6 py-2"
+    tag="a"
+    href="https://buy.stripe.com/7sI9CH0x17bb6aYeUV"
+    target="_blank"
+    rel="noopener"
+  >
+    Buy Now for $149
+  </UButton>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
-
-const isModalOpen = ref(false)
 </script> 
